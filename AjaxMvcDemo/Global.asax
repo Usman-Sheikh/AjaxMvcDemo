6b460834f4419c01d9e915d23cc01edf6d29ee1b
Usman-Sheikh <sh.usmanikram@hotmail.com>
2016-03-26 12:59:54 +0500

    Initial Commit

---
+++ AjaxMvcDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AjaxMvcDemo.MvcApplication" Language="C#" %>
---
+++ AjaxMvcDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AjaxMvcDemo.Startup))]
+namespace AjaxMvcDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
